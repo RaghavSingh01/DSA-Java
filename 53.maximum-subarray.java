1c4170349ba63d28da96ec041bfd314e73bd481a
Raghav <singh.raghav1013@gmail.com>
2026-08-21 17:15:31 +0530

    DSA DAY 6 redone kedan's algo

--- 53.maximum-subarray.java
+++ 53.maximum-subarray.java
@@ -7,17 +7,22 @@
 // @lc code=start
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        for(int i = 0; i < nums.length; i++){
-                if(sum >= 0){
-                    sum += nums[i];
-                    maxSum = Math.max(maxSum, sum);
-                }
-                else if (sum < 0){
-                    sum = nums[i];
-                    maxSum = Math.max(maxSum, sum);
-                }
+        int sum  = 0;
+        int maxSum = nums[0];
+        int i = 0;
+
+        while(i < nums.length){
+            if(sum < 0 && nums[i] >= 0){
+                sum = nums[i];
+                maxSum = Math.max(sum, maxSum);    
+            }else{
+                sum += nums[i];
+                maxSum = Math.max(Math.max(sum,nums[i]), maxSum);
+            }
+            if(sum < maxSum && sum < 0){
+                sum = 0; 
+            }
+           i++;
         }
         return maxSum;
     }
